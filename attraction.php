c92e9c1ec088464a9d9c1917a94a8cd8f5032cd0
iddhi-sulakshana <iddhidasanayaka@gmail.com>
2022-03-04 23:34:56 +0530

    Finished Attraction Page and Attraction View Page

--- attraction.php
+++ attraction.php
@@ -1,286 +1,39 @@
-<?php include 'head.php' ?>
+<?php
+	$sql = "SELECT `attractionId`, `attractionTitle`, `attractionImageHome` FROM `table_attraction`";
+	include 'dbcon.php';
+	$conn = OpenCon();
+	$result = $conn->query($sql);
+	include 'head.php'
+?>
   <title>Atricles</title>
 	<link rel="stylesheet" href="css/attraction.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
  <?php include 'header.php' ?>
 <section class="locations">
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/arthur.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Arthur's Seat</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/anuradapura.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Anuradhapura</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/attidiya.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Attidiya Bird Sanctuary</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/babarakanda.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Babarakanda Waterfall</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/bahiravakanda.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Bahiravakanda</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/balapitiya.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Balapitiya Beach</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/bentota lake.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Bentota Lake</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/mahamevna.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Mahamevnawa</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/bere.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Beira Lake</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/bumuseum.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Buddhist Museum</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/cricket.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Cricket Museum</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/dabulla_cave.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Dambulla</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/dhowa.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Dhowa</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/doctor.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>The Doctors House</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/ella.jpg">
-			<div class="layer">
-				<h3>Ella</h3>
-				<a href="attraction_view.php"><h3>Ella</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/lighthouse.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Galle Fort</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/galleface.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Galle Face Green</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/gangaramaya.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Gangaramaya Temple</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/horton.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Horton Plains</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/inependence.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Independence Square</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/kande vihara.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Kande Viharaya</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/kandy.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Kandy</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/koggala.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Koggala</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/mahiyanganaya.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Sorabora Lake</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/mirissa.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Mirissa</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/moragalla.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Moragalla</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/museum.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>National Museum</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/pinnawala.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Pinnawala</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/panadura.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Panadura Beach</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/polhena.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Polhena Beach</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/pollonnaruwa_3.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Polonnaruwa</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/sinharaja.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Sinharaja forest</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">			
-			<img src="img/Attraction/Home/sigiriya.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Sigiriya</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
-	<div class="row">
-		<div class="locations-col">
-			<img src="img/Attraction/Home/adams.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Adam's Rock</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/worldsend.webp">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>World's End</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>
-		<div class="locations-col">
-			<img src="img/Attraction/Home/unawatuna.jpg">
-			<div class="layer">
-				<a href="attraction_view.php"><h3>Unawatuna Beach</h3></a>
-				<a href="attraction_view.php"><h4>read more</h4></a>
-			</div>
-		</div>		
-	</div>
+		<?php
+			$count = 0;
+			foreach($result as $item){
+				if($count == 0){
+					echo '<div class="row">';
+				}	
+			?>
+				<div class="locations-col">
+				<a href="attraction_view.php?attractionId=<?php echo $item['attractionId'] ?>">
+					<img src="img/Attraction/Home/<?php echo $item['attractionImageHome'] ?>">
+					<div class="layer">
+						<h3><?php echo $item['attractionTitle'] ?></h3>
+						<h4>read more</h4>
+					</div>
+				</a>
+				</div>
+			<?php	
+				$count++;
+				if($count == 3){
+					echo '</div>';
+					$count = 0;
+				}
+			}
+		?>
 	<?php include 'footer.php' ?>
 	<script src="js/main.js"></script>
 </body>
--- attraction_view.php
+++ attraction_view.php
@@ -1,12 +1,33 @@
-<?php include 'head.php'; ?>
-  <title>Aurthors Seat</title>
+<?php
+	include 'dbcon.php';
+	if(is_null($_REQUEST['attractionId']) || $_REQUEST['attractionId'] <= 0){
+		header('Location: vehicle_search.php');
+		exit();
+	}
+	$aid = htmlspecialchars($_REQUEST['attractionId']);
+	
+	$sqlattraction = "SELECT * FROM `table_attraction` WHERE `attractionId` = " . $aid;
+	
+	$conn = OpenCon();
+
+	$resultattraction = $conn->query($sqlattraction);
+	
+	CloseCon($conn);
+	if($resultattraction->num_rows < 1){
+		header('Location: attraction.php');
+		exit();
+	}
+	$resultattraction = $resultattraction->fetch_assoc();
+	include 'head.php';
+?>
+  <title><?php echo $resultattraction['attractionTitle'] ?></title>
 	<link rel="stylesheet" href="css/attraction.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<style>
 		.header{
 			min-height: 70vh;
 			width: 100%;
-			background-image: linear-gradient(rgba(4,9,30,0.5),rgba(4,9,30,0.2)),url("img/Attraction/arthur.jpg");
+			background-image: linear-gradient(rgba(4,9,30,0.5),rgba(4,9,30,0.2)),url("img/Attraction/<?php echo $resultattraction['attractionImagePri'] ?>");
 			background-position: center;
 			background-size: cover;
 			position: relative;
@@ -16,25 +37,16 @@
 
 	<section class="header">	
 	<div class="text-box">
-	<h1>Arthur's Seat</h1>
+	<h1><?php echo $resultattraction['attractionTitle'] ?></h1>
 	</div>
 	</section>
 <!--content-->
-	<section>
-		<hr>
-	<p>Located just southwest of Kandy Lake, Arthur's Seat is one of those places in Sri Lanka that is popular amongst photographers to capture natural beauty. <br><br>The place has an interesting history about how Arthur used to view the city by standing on a stone seat under a tree located in his bungalow.
-	<br><br>From here one can witness the beautiful view of Kandy Lake, Dalada Maligawa, the Temple of the Tooth, and Trincomalee street.</p>
-	<br>
-		<hr>
-	<b>Highlights : </b>Witness the former palace of the Royal family. ,Explore the Buddha on top of a hill
-	<br><br>
-	<b>Timings : </b>Open 24 hours
-	<hr>
-	<br><br><br>
-	</section>
+		<br>
+	<section><?php echo $resultattraction['attractionDescription'] ?></section>
+		<br>
 	
 	<section style="width: 100%;">
-		<iframe style="width: 100%;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.577493014535!2d80.63761661453533!3d7.288816994739183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae3662f177fedd7%3A0xe9f7bd6ebfa0619f!2sArthur&#39;s%20Seat%2C%20Kandy!5e0!3m2!1sen!2slk!4v1641398160155!5m2!1sen!2slk" width="1150" height="550" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+		<iframe style="width: 100%;" src="<?php echo $resultattraction['attractionLocationLink'] ?>" width="1150" height="550" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 	</section>
 	<section class="cta">
 		<a href="attraction.php" class="back-btn">BACK</a>
